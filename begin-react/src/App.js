ac1008acd9f6c1cc174562d43626ba9cc99dc3f2
Jinhyeok Kil <roadjh0201@gmail.com>
2021-06-07 13:33:06 +0900

    1-13. 배열에 항목 추가하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,32 +1,71 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import CreateUser from './CreateUser';
 import UserList from './UserList';
 
+const initialUsers = [
+	{
+		id: 1,
+		username: 'velopert',
+		email: 'public.velopert@gmail.com',
+	},
+	{
+		id: 2,
+		username: 'tester',
+		email: 'tester@example.com',
+	},
+	{
+		id: 3,
+		username: 'liz',
+		email: 'liz@example.com',
+	},
+];
+
 function App() {
-	const users = [
-		{
-			id: 1,
-			username: 'velopert',
-			email: 'public.velopert@gmail.com',
-		},
-		{
-			id: 2,
-			username: 'tester',
-			email: 'tester@example.com',
-		},
-		{
-			id: 3,
-			username: 'liz',
-			email: 'liz@example.com',
-		},
-	];
+	const [inputs, setInputs] = useState({
+		username: '',
+		email: '',
+	});
+
+	const { username, email } = inputs;
+
+	const onChange = (e) => {
+		const { name, value } = e.target;
+		setInputs({
+			...inputs,
+			[name]: value,
+		});
+	};
+
+	const [users, setUsers] = useState(initialUsers);
 
 	const nextId = useRef(4);
 	const onCreate = () => {
-		// 나중에 구현 할 배열에 항목 추가하는 로직
-		// ...
+		const user = {
+			id: nextId.current,
+			username,
+			email,
+		};
+
+		setUsers([...users, user]);
+		// setUsers(users.concat(user));
+
+		setInputs({
+			username: '',
+			email: '',
+		});
 		nextId.current += 1;
 	};
-	return <UserList users={users} />;
+	return (
+		<>
+			<CreateUser
+				username={username}
+				email={email}
+				onChange={onChange}
+				onCreate={onCreate}
+			/>
+			<UserList users={users} />
+		</>
+	);
 }
 
 export default App;
