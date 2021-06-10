dfea6e50bda0d688405289c43b9b0d6e987808e8
Jinhyeok Kil <roadjh0201@gmail.com>
2021-06-10 13:08:24 +0900

    1-22. Context API를 사용한 전역 값 관리

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -55,6 +55,8 @@ function reducer(state, action) {
 	}
 }
 
+export const UserDispatch = React.createContext(null);
+
 function App() {
 	const [{ username, email }, onChange, reset] = useInputs({
 		username: '',
@@ -79,33 +81,19 @@ function App() {
 		reset();
 	}, [username, email, reset]);
 
-	const onToggle = useCallback((id) => {
-		dispatch({
-			type: 'TOGGLE_USER',
-			id,
-		});
-	}, []);
-
-	const onRemove = useCallback((id) => {
-		dispatch({
-			type: 'REMOVE_USER',
-			id,
-		});
-	}, []);
-
 	const count = useMemo(() => countActiveUsers(users), [users]);
 
 	return (
-		<>
+		<UserDispatch.Provider value={dispatch}>
 			<CreateUser
 				username={username}
 				email={email}
 				onChange={onChange}
 				onCreate={onCreate}
 			/>
-			<UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+			<UserList users={users} />
 			<div>활성사용자 수 : {count}</div>
-		</>
+		</UserDispatch.Provider>
 	);
 }
 
