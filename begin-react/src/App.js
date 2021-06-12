7bcd096a2725e39478417d55ac8849cafee50ad3
Jinhyeok Kil <roadjh0201@gmail.com>
2021-06-12 12:05:09 +0900

    1-23. Immer를 사용한 더 쉬운 불변성 관리

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,6 +1,7 @@
 import React, { useMemo, useReducer } from 'react';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
+import produce from 'immer';
 
 const initialState = {
 	users: [
@@ -33,22 +34,19 @@ function countActiveUsers(users) {
 function reducer(state, action) {
 	switch (action.type) {
 		case 'CREATE_USER':
-			return {
-				inputs: initialState.inputs,
-				users: state.users.concat(action.user),
-			};
+			return produce(state, (draft) => {
+				draft.users.push(action.user);
+			});
 		case 'TOGGLE_USER':
-			return {
-				...state,
-				users: state.users.map((user) =>
-					user.id === action.id ? { ...user, active: !user.active } : user
-				),
-			};
+			return produce(state, (draft) => {
+				const user = draft.users.find((user) => user.id === action.id);
+				user.active = !user.active;
+			});
 		case 'REMOVE_USER':
-			return {
-				...state,
-				users: state.users.filter((user) => user.id !== action.id),
-			};
+			return produce(state, (draft) => {
+				const index = draft.users.findIndex((user) => user.id === action.id);
+				draft.users.splice(index, 1);
+			});
 		default:
 			return state;
 	}
