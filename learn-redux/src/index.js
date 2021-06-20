89fcfebcb3d45c1f7750b8a2d1876c3d99548e45
Jinhyeok Kil <roadjh0201@gmail.com>
2021-06-20 16:27:51 +0900

    6-4. 리덕스 모듈 만들기

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './App';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
