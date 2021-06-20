798ed39f2726a000a8439e0e67b33d124bb13aa8
Jinhyeok Kil <roadjh0201@gmail.com>
2021-06-20 15:01:21 +0900

    5-5. useReactRouter Hook 사용하기

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, NavLink } from 'react-router-dom';
 import Profile from './Profile';
 import WithRouterSample from './WithRouterSample';
+import RouterHookSample from './RouterHookSample';
 
 function Profiles() {
 	return (
@@ -33,6 +34,7 @@ function Profiles() {
 			/>
 			<Route path='/profiles/:username' component={Profile} />
 			<WithRouterSample />
+			<RouterHookSample />
 		</div>
 	);
 }
