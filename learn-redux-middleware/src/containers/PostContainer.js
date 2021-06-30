f159bc6988a756320f799b8e4f82b98cbd52a465
Jinhyeok Kil <roadjh0201@gmail.com>
2021-06-30 20:42:50 +0900

    7-6. API 재로딩 문제 해결하기(중요)

--- learn-redux-middleware/src/containers/PostContainer.js
+++ learn-redux-middleware/src/containers/PostContainer.js
@@ -4,14 +4,23 @@ import { getPost } from '../modules/posts';
 import Post from '../components/Post';
 
 function PostContainer({ postId }) {
-	const { data, loading, error } = useSelector((state) => state.posts.post);
+	const { data, loading, error } = useSelector(
+		(state) => state.posts.post[postId]
+	) || {
+		loading: false,
+		data: null,
+		error: null,
+	}; // 아예 데이터가 존재하지 않을 때가 있으므로, 비구조화 할당이 오류나지 않도록
 	const dispatch = useDispatch();
 
 	useEffect(() => {
+		if (data) return; // 포스트가 존재하면 아예 요청을 하지 않음
 		dispatch(getPost(postId));
-	}, [postId, dispatch]);
+	}, [postId, dispatch, data]);
 
 	if (loading) return <div>로딩중...</div>;
+	// 요청은 하되 로딩중은 보여주지 않는 방식
+	//  if (loading && !data) return <div>로딩중...</div>;
 	if (error) return <div>에러 발생!</div>;
 	if (!data) return null;
 
@@ -19,3 +28,6 @@ function PostContainer({ postId }) {
 }
 
 export default PostContainer;
+
+// 데이터 캐싱을 위해서는 요청 자체를 하지않는 현재 방식
+// 포스트정보가 바뀔 가능성이 있다면 요청은 하되 사용자경험을 위해 로딩중을 표시하지 않는 형태로 구현
--- learn-redux-middleware/src/modules/posts.js
+++ learn-redux-middleware/src/modules/posts.js
@@ -1,7 +1,9 @@
 import * as postsAPI from '../api/posts'; // 모두 불러오기
 import {
 	createPromiseThunk,
+	createPromiseThunkById,
 	handleAsyncActions,
+	handleAsyncActionsById,
 	reducerUtils,
 } from '../lib/asyncUtils';
 
@@ -16,15 +18,19 @@ const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
+// 포스트 비우기
+const CLEAR_POST = 'CLEAR_POST';
+
 // thunk 를 사용 할 때, 꼭 모든 액션들에 대하여 액션 생성함수를 만들 필요는 없습니다.
 // 그냥 thunk 함수에서 바로 액션 객체를 만들어주어도 괜찮습니다.
-
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
+
+export const clearPost = () => ({ type: CLEAR_POST });
 
 const initialState = {
 	posts: reducerUtils.initial(),
-	post: reducerUtils.initial(),
+	post: {},
 };
 
 export default function posts(state = initialState, action) {
@@ -32,11 +38,18 @@ export default function posts(state = initialState, action) {
 		case GET_POSTS:
 		case GET_POSTS_SUCCESS:
 		case GET_POSTS_ERROR:
-			return handleAsyncActions(GET_POSTS, 'posts')(state, action);
+			return handleAsyncActions(GET_POSTS, 'posts', true)(state, action);
 		case GET_POST:
 		case GET_POST_SUCCESS:
 		case GET_POST_ERROR:
-			return handleAsyncActions(GET_POST, 'post')(state, action);
+			return handleAsyncActionsById(GET_POST, 'post')(state, action);
+		// 요청은 하되 로딩중은 보여주지 않는 방식
+		// return handleAsyncActionsById(GET_POST, 'post', true)(state, action);
+		case CLEAR_POST:
+			return {
+				...state,
+				post: reducerUtils.initial(),
+			};
 		default:
 			return state;
 	}
